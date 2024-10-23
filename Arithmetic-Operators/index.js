fd305265eba3e68af4d3ad2796ce8a852a8a3e2b
unknown <s.r.mahamid@gmail.com>
2024-10-23 20:55:35 +0500

    Arithmetic operators

---
+++ Arithmetic-Operators/index.js
@@ -0,0 +1,41 @@
+// Arithmetic Operators
+// ( + , - , * , / , ** . % )
+
+let num = 5;
+num += 1;
+console.log(num);
+
+num = 5;
+num -= 1;
+console.log(num);
+
+num = 5;
+num *= 2;
+console.log(num);
+
+num = 10;
+num /= 2;
+console.log(num);
+
+num = 10;
+num **= 2;
+console.log(num);
+
+num = 10;
+num %= 3;
+console.log(num);
+// increment
+num = 10;
+num++;
+console.log(num);
+// decrement
+num = 10;
+num--;
+console.log(num);
+
+/* Operators Precdence
+   Parenthesis
+   exponents
+   multiplication / division
+   addition / subtraction
+   */
