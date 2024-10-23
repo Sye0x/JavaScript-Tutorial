6aaadedd5006060aab340eb6cfdd8b5182be80b6
unknown <s.r.mahamid@gmail.com>
2024-10-23 20:10:15 +0500

    Intro

---
+++ intro/index.js
@@ -0,0 +1,9 @@
+console.log("Hello");
+window.alert("This is an alert");
+document.getElementById("heading").textContent = "Hello World";
+// This is a comment
+/* This
+    is
+    a
+    multiline
+    comment */
