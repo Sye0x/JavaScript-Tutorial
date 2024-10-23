1b53174e0f9df91cfee52624afafa4c9984c5015
unknown <s.r.mahamid@gmail.com>
2024-10-23 20:41:36 +0500

    Variables

---
+++ Variable/index.js
@@ -0,0 +1,22 @@
+// Variable intro
+
+// let x; decleration
+// x=100; assignment
+
+let gpa = 3.53;
+console.log(gpa);
+let age = 21;
+console.log(`I am ${age} years old`);
+let Name = "Raza";
+console.log(typeof Name);
+console.log(typeof gpa);
+let online = true;
+console.log(`${Name} is online: ${online}`);
+
+let fullname = "Syed Mohamid Raza";
+let cgpa = 3.53;
+let isenroled = true;
+
+document.getElementById("h1").textContent = `Name: ${fullname}`;
+document.getElementById("h2").textContent = `CGPA: ${cgpa}`;
+document.getElementById("h3").textContent = `Enrolled: ${isenroled}`;
