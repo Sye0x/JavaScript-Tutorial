b880c3daa044f07fa578daf2afa707de448636c0
unknown <s.r.mahamid@gmail.com>
2024-10-27 23:18:30 +0500

    Logical Operator

---
+++ 15.Logical-Operators/index.js
@@ -0,0 +1,26 @@
+// && ,  || , !
+
+let age = 50;
+
+if (age >= 18 && age < 100) {
+  console.log("You can travel Alone");
+} else {
+  console.log("You can not travel Alone");
+}
+
+let isStudent = true;
+let isTeacher = false;
+
+if (isStudent === true || isTeacher === true) {
+  console.log("You can enter the university");
+} else {
+  console.log("You can not enter the university");
+}
+
+let num = 3;
+
+if (num % 2 !== 0) {
+  console.log(`${num} is an odd number`);
+} else {
+  console.log(`${num} is not an odd number`);
+}
