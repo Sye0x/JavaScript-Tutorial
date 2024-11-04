1131ff80d51cf36d182e9229b235349469905f36
unknown <s.r.mahamid@gmail.com>
2024-11-04 23:44:18 +0500

    Added Comments

--- 31.Objects/index.js
+++ 31.Objects/index.js
@@ -1,11 +1,17 @@
+// Define an object 'pikachu' with properties and a method
 const pikachu = {
-  name: "Pikachu",
-  type: "Electric",
+  name: "Pikachu", // Name of the character
+  type: "Electric", // Type of the character
   usethunderbolt: () => {
-    console.log("Pikachuuuu");
+    console.log("Pikachuuuu"); // Method that prints "Pikachuuuu" to the console
   },
 };
 
+// Log the name of 'pikachu' to the console
 console.log(pikachu.name);
+
+// Log the type of 'pikachu' to the console
 console.log(pikachu.type);
+
+// Call the 'usethunderbolt' method of 'pikachu'
 pikachu.usethunderbolt();
