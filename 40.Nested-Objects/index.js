2d520b40ba34a1d5f47a7bd4799731a2d439519d
unknown <s.r.mahamid@gmail.com>
2024-11-10 18:36:05 +0500

    Nested Objects

--- 40.Nested-Objects/index.js
+++ 40.Nested-Objects/index.js
@@ -1,4 +1,37 @@
-const person = {
+class Person {
+  constructor(name, age, ...address) {
+    this.name = name;
+    this.age = age;
+    this.address = new Address(...address);
+  }
+  display() {
+    console.log(`Name:${this.name}`);
+    console.log(`Age:${this.age}`);
+    for (const loc in this.address) {
+      console.log(this.address[loc]);
+    }
+  }
+}
+
+class Address {
+  constructor(street, city, country) {
+    this.street = street;
+    this.city = city;
+    this.country = country;
+  }
+}
+
+const person1 = new Person(
+  "Spongebob",
+  30,
+  "124 Conch St.",
+  "Bikini Bottom",
+  "Int. Waters"
+);
+
+person1.display();
+
+const person2 = {
   fullName: "Spongebob Squarepants",
   age: 30,
   isStudent: true,
@@ -10,12 +43,6 @@ const person = {
   },
 };
 
-console.log(person.fullName);
-console.log(person.age);
-console.log(person.isStudent);
-for (const hobby in person.hobbies) {
-  console.log(person.hobbies[hobby]);
-}
-for (const property in person.address) {
-  console.log(person.address[property]);
+for (const property in person2.address) {
+  console.log(person2.address[property]);
 }
