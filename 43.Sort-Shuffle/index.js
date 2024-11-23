0ff44bc9140689df8a37eee2e56772baded3d86a
unknown <s.r.mahamid@gmail.com>
2024-11-23 19:42:24 +0500

    Shuffle

--- 43.Sort-Shuffle/index.js
+++ 43.Sort-Shuffle/index.js
@@ -11,3 +11,15 @@ people.sort((a, b) => b.name.localeCompare(a.name));
 console.log(people);
 people.sort((a, b) => a.age - b.age);
 console.log(people);
+
+// Fisher-Yates algorithm
+const cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+shuffle(cards);
+console.log(cards);
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+}
